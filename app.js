5332ab23c6dcf6411f65407c434d87e3b910f867
GABRIEL LACERDA CORREIA <25132902@dsn127m1242316.cfp127aluno.local>
2025-08-11 11:17:54 -0300

    Criando galeria com HTML, CSS e JavaScript

---
+++ app.js
@@ -0,0 +1,72 @@
+"use strict"
+
+const imagens = [
+    {
+        nome: 'barcos',
+        url: './img/barcos.jpg'
+    },
+
+    {
+        nome: 'canoa',
+        url: './img/canoa.jpg'
+    },
+
+    {
+        nome: 'flores',
+        url: './img/flores.jpg'
+    },
+
+    {
+        nome: 'havai',
+        url: './img/havai.jpg'
+    },
+
+    {
+        nome: 'ilha',
+        url: './img/ilha.jpg'
+    },
+
+    {
+        nome: 'los_angeles',
+        url: './img/los_angeles.jpg'
+    },
+
+    {
+        nome: 'noite',
+        url: './img/noite.jpg'
+    },
+
+    {
+        nome: 'restaurante',
+        url: './img/restaurante.jpg'
+    },
+
+    {
+        nome: 'trabalho',
+        url: './img/trabalho.jpg'
+    }
+
+]
+
+function criarImagem(srcImagem) {
+    const galeria = document.getElementById('galeria')
+    const imagem = document.createElement('img')
+    imagem.src = srcImagem.url
+    galeria.appendChild(imagem)
+
+}
+
+function carregarImagens() {
+    
+    // for (let qtd of imagens) {
+    //     const imagem = document.createElement('img')
+    //     imagem.src = imagens[imagens.indexOf(qtd)]
+
+    //     galeria.appendChild(imagem)
+    // }
+    
+    imagens.forEach(criarImagem)
+
+}
+
+carregarImagens()
